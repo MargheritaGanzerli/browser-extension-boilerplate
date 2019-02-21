4cd2b3be941f6cc815c551d5ce33cc820d985f00
MargheritaGanzerli <margherita.ganzerli@gmail.com>
2019-02-21 14:18:37 +0100

    New

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // Listen for messages
+const body = document.querySelector("body");
 browser.runtime.onMessage.addListener(receiver);
 
 // A message is received
 function receiver(request, sender, sendResponse) {
   if (request.active) {
     //do something
+      body.classList.toggle("my-beautiful-extension");
   } else {
     //do else
+      body.classList.toggle("my-beautiful-extension");
   }
 }
